dec6dba962c2215091491d179cfa76eedc1aff7c
Tautvydas-coder <b7&*%13kHqvP>
2022-08-08 13:55:06 +0300

    added major delivery centers value test

--- PyTest/test_main.py
+++ PyTest/test_main.py
@@ -1,25 +1,39 @@
+import time
+
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from resources.variables import *
 from driver_services.service import *
+from selenium.webdriver.support import expected_conditions as EC
 
 # driver = driver_service_mozila()
 driver = driver_service_chrome()
 
 
 def setup():
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(7)
     driver.maximize_window()
     driver.get(ORION_WEB)
     driver.find_element(By.ID, Cookies).click()
 
+# def test_HeadersDropdownButton():
+#     header = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,"/html/body/header")))
+#     element = driver.find_element(By.ID, COMPANY)
+#     dropdown = driver.find_element(By.LINK_TEXT, CAREERS)
+#     ActionChains(driver).move_to_element(header).move_to_element(element).move_to_element(dropdown).click().perform()
+#     web_title = driver.title
+#     assert web_title == "Careers - Orion Innovation"
 
-def test_HeadersDropdownButton():
-    header = driver.find_element(By.ID, COMPANY)
-    dropdown = driver.find_element(By.LINK_TEXT, CAREERS)
-    ActionChains(driver).move_to_element(header).click(dropdown).perform()
-    web_title = driver.title
-    assert web_title == "Careers - Orion Innovation"
+
+
+def test_MajorDeliveryCentersValue():
+    MaturityAndScale = driver.find_element(By.XPATH,'//*[@id="h-maturity-scale"]')
+    ActionChains(driver).scroll_to_element(MaturityAndScale).perform()
+    element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="number-block-block_5defc5f643e72"]/div[1]')))
+    time.sleep(3)
+    number = element.text
+    assert number == "14"
 
 # def teardown():
 #     driver.close()
