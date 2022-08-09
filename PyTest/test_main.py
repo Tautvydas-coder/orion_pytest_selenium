c75c8432cffbefec14bfd48cf3c3f29d3971abdc
Tautvydas-coder <b7&*%13kHqvP>
2022-08-09 12:24:28 +0300

    added WebDriverWaiting

--- PyTest/test_main.py
+++ PyTest/test_main.py
@@ -15,7 +15,7 @@ def setup():
     global driver
     driver = driver_service_chrome()
     # driver = driver_service_mozila()
-    driver.implicitly_wait(7)
+    driver.implicitly_wait(10)
     driver.maximize_window()
     driver.get(ORION_WEB)
     time.sleep(1)
@@ -35,18 +35,18 @@ def test_headersDropdownButton(setup):
     dropdown = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.LINK_TEXT, CAREERS)))
     ActionChains(driver).move_to_element(header).move_to_element(element).perform()
     driver.implicitly_wait(10)
-    ActionChains(driver).move_to_element(dropdown).click(dropdown).perform()
-    driver.implicitly_wait(10)
+    ActionChains(driver).move_to_element(dropdown).perform()
+    ActionChains(driver).click(dropdown).perform()
+    time.sleep(3)
     web_title = driver.title
     assert web_title == CAREERS_WEB_TITLE
 
 
 @pytest.mark.smoke
 def test_majorDeliveryCentersValue(setup):
-    info_block = driver.find_element(By.XPATH, MATURITY_SCALE)
+    info_block = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, MATURITY_SCALE)))
     ActionChains(driver).scroll_to_element(info_block).perform()
-    element = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.XPATH, NUM_OF_CENTERS_ELEMENT)))
+    element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, NUM_OF_CENTERS_ELEMENT)))
     time.sleep(2)
     number = element.text
     assert number == CORRECT_NUMBER
@@ -54,13 +54,13 @@ def test_majorDeliveryCentersValue(setup):
 
 @pytest.mark.regression
 def test_countrySelection(setup):
-    field = driver.find_element(By.XPATH, AGREE_TO_COMMUNICATE)
-    select_element = driver.find_element(By.XPATH, COUNTRIES)
+    field = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, AGREE_TO_COMMUNICATE)))
+    select_element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, COUNTRIES)))
     ActionChains(driver).scroll_to_element(field).perform()
     ActionChains(driver).move_to_element(select_element).click().perform()
-    selected_country = driver.find_element(By.XPATH, COUNTRY)
+    selected_country = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, COUNTRY)))
     ActionChains(driver).move_to_element(selected_country).click().perform()
-    country = driver.find_element(By.XPATH, SELECTED_COUNTRY)
+    country = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, SELECTED_COUNTRY)))
     assert country.text == TESTING_COUNTRY
 
 
@@ -68,11 +68,11 @@ def test_countrySelection(setup):
 def test_uploadCV(setup):
     driver.get(APPLY_FOR_JOB)
     time.sleep(2)
-    save_info_check = driver.find_element(By.XPATH, SAVE_INFO)
+    save_info_check = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, SAVE_INFO)))
     ActionChains(driver).scroll_to_element(save_info_check).perform()
-    upload = driver.find_element(By.XPATH, RESUME)
+    upload = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, RESUME)))
     upload.send_keys(CV_PATH)
-    cv_name = driver.find_element(By.XPATH, UPLOADED_DESCRIPTION)
+    cv_name = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, UPLOADED_DESCRIPTION)))
     cv_name_text = cv_name.text
     assert cv_name_text.__contains__(CV_NAME)
 
@@ -80,10 +80,11 @@ def test_uploadCV(setup):
 @pytest.mark.smoke
 def test_searchBar(setup):
     header = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, HEADER)))
-    search_button = driver.find_element(By.XPATH, HEADER_SEARCH_BUTTON)
+    search_button = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, HEADER_SEARCH_BUTTON)))
     ActionChains(driver).move_to_element(header).click(search_button).perform()
-    driver.find_element(By.XPATH, SEARCH_INPUT).send_keys(SEARCING_NAME)
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, SEARCH_BUTTON))).click()
-    time.sleep(1)
-    text = driver.find_element(By.XPATH, SEARCH_MATCHES).text
-    assert text.__contains__("matches for " + SEARCING_NAME)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, SEARCH_INPUT))).send_keys(SEARCING_NAME)
+    WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, SEARCH_BUTTON))).click()
+    # time.sleep(2)
+    matches = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, SEARCH_MATCHES)))
+    matches_for = matches.text
+    assert matches_for.__contains__("matches for " + SEARCING_NAME)
